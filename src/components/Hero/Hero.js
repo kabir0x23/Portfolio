87afbe7dc5fc12dda795e0240995647176580e32
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-29 16:40:36 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {Section, SectionText, SectionTitle} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {LeftSection, BackgroundImage} from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+    <>
+            <Section row nopadding>
+                <LeftSection>
+                    <SectionTitle main center>
+                        Welcome to <br/>
+                        My Personal Portfolio
+                    </SectionTitle>
+                    <SectionText>
+                        I am Cyber Security Enthusiast, a Full Stack Web Developer and a Programmer
+                    </SectionText>
+                    <Button onClick={() => window.location = 'https://linktree.com/kabir0x23'}>Learn More</Button>
+                </LeftSection>
+            </Section>
+    </>
 );
 
 export default Hero;
