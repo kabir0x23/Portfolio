e45c0eff5e1860831e176576915d19859520a80f
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-29 16:47:06 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,91 @@
-export const projects = [
-  {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 0,
-  },
-  {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 1,
-  },
-  {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
-  },
-  {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
-  },
+export const webProjects = [
+    {
+        title: 'Thecyberhub',
+        description: "Thecyberhub is a community website of thecyberworld community.",
+        image: '/images/Projects/ThecyberhubOrg.png',
+        tags: ['React'],
+        source: 'https://github.com/kabir0x23/NewsFlash',
+        visit: 'https://thecyberhub.org',
+        id: 0,
+    }, {
+        title: 'Cryptoverse',
+        description: "Crypto news, Cryptocurrencies, Coin details.",
+        image: '/images/Projects/Cryptoverse.png',
+        tags: ['React', 'API', 'Antd'],
+        // source: 'https://github.com/kabir0x23/NewsFlash',
+        visit: 'https://kabir0x23.github.io/NewsFlash/',
+        id: 1,
+    }, {
+        title: 'NewsFlash',
+        description: "NewsFlash is News Website, NewsFlash will going to provide News using api in future.",
+        image: '/images/Projects/NewsFlash.png',
+        tags: ['Html', 'CSS', 'JavaScript'],
+        source: 'https://github.com/kabir0x23/NewsFlash',
+        visit: 'https://kabir0x23.github.io/NewsFlash/',
+        id: 2,
+    },
 ];
 
+export const web3Projects = [
+    {
+        title: 'Cryptoverse Web3',
+        description: "A Blockchain based web3 application. Provides Secure transactions with Low fees, Crypto details, and Crypto news",
+        image: '/images/projects_Web3/CryptoverseWeb3_Project_Image.png',
+        tags: ['React', 'Blockchain', 'Solidity'],
+        // source: 'https://github.com/cryptoverseWeb3/cryptoverseWeb3',
+        visit: 'https://cryptoverseWeb3.com',
+        id: 0,
+    },
+];
+
+export const cyberProjects = [
+    {
+        title: 'NewsFlash',
+        description: "NewsFlash is News Website, NewsFlash will going to provide News using api in future.",
+        image: '/images/ImgThecyberhub.org.png',
+        tags: ['Html', 'CSS', 'JavaScript'],
+        source: 'https://github.com/kabir0x23/NewsFlash',
+        visit: 'https://kabir0x23.github.io/NewsFlash/',
+        id: 0,
+    },
+    {
+        title: 'Cryptoverse',
+        description: "Crypto news, Cryptocurrencies, Coin details.",
+        image: '/images/cryptoverse.png',
+        tags: ['Html', 'CSS', 'JavaScript'],
+        source: 'https://github.com/kabir0x23/NewsFlash',
+        visit: 'https://kabir0x23.github.io/NewsFlash/',
+        id: 1,
+    },
+]
+
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+    {year: 2017, text: 'Started Diploma in Computer Science',},
+    {
+        year: 2020,
+        text: <ul>
+            <li>1. Learned Html, CSS, Javascript, PHP, and SQL.</li>
+            <li>2. Built a Blog Website with PHP</li>
+            <li>3. Started Learning about Cyber Security.</li>
+            <li>4. Founded <a href={"https://www.linktree.com/thecyberworld"}>TheCyberWorld</a>.</li>
+            <li>5. Started B-TECH.</li>
+        </ul>
+    },
+    {
+        year: 2021,
+        text: <ul>
+            <li>1. Learned Python</li>
+            <li>2. Started building <a href={"https://www.linktree.com/thecyberworld"}>thecyberworld</a> community</li>
+            <li>3. Guided 20,000+ people.</li>
+        </ul>
+    },
+    {
+        year: 2022, text:
+            <ul>
+                <li>1. Started learning about MERN Stack, DevOps and AI</li>
+                <li>2. Built NewsFlash.</li>
+                <li>2. Built <a href={"https://www.linktree.com/cryptoverseweb3"}>CryptoverseWeb3</a>.</li>
+
+            </ul>
+    },
 ];
