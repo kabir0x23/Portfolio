f54fe0dbf7573e71c80407162cd5ccf388392708
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-01 15:52:28 +0530

    Update Accomplishments.js

--- src/components/Accomplishments/Accomplishments.js
+++ src/components/Accomplishments/Accomplishments.js
@@ -20,7 +20,7 @@ const data1 = [
     {number: 0, text: 'LinkedIn Followers'},
     {number: 0, text: 'YouTube Subscribers'},
     {number: 0, text: 'Twitter Followers'},
-    {number: 0,  text: 'Discord Community'},
+    {number: 0, text: 'Discord Community'},
     {number: 0, text: 'Telegrams Members'},
     {number: 0, text: 'Instagram Followers'}
 ]
@@ -33,49 +33,51 @@ const data2 = [
     {number: 100, text: 'LinkedIn Followers'},
     {number: 1200, text: 'YouTube Subscribers'},
     {number: 1700, text: 'Twitter Followers'},
-    {number: 2500,  text: 'Discord Community'},
+    {number: 2500, text: 'Discord Community'},
     {number: 12000, text: 'Telegrams Members'},
     {number: 70000, text: 'Instagram Followers'}
 ]
 
 const Accomplishments = () => (
     <>
-    <Section>
-        <SectionTitle>Personal Accomplishments</SectionTitle>
-        <Boxes>
-            {data.map((card, index) => (
-                <Box key={index}>
-                    <BoxNum>{card.number}+</BoxNum>
-                    <BoxText>{card.text}</BoxText>
-                </Box>
-            ))}
-        </Boxes>
-    </Section>
-<Section>
-    <SectionDivider divider />
-    <SectionTitle>CryptoverseWeb3 Community Accomplishments</SectionTitle>
-    <title></title>
-    <Boxes>
-        {data1.map((card, index) => (
-            <Box key={index}>
-                <BoxNum>{card.number}+</BoxNum>
-                <BoxText>{card.text}</BoxText>
-            </Box>
-        ))}
-    </Boxes>
-</Section>
         <Section>
-    <SectionDivider divider />
-    <SectionTitle>thecyberw0rld Community Accomplishments</SectionTitle>
-    <Boxes>
-        {data2.map((card, index) => (
-            <Box key={index}>
-                <BoxNum>{card.number}+</BoxNum>
-                <BoxText>{card.text}</BoxText>
-            </Box>
-        ))}
-    </Boxes>
-</Section>
+            <SectionTitle>Personal Accomplishments</SectionTitle>
+            <Boxes>
+                {data.map((card, index) => (
+                    <Box key={index}>
+                        <BoxNum>{card.number}+</BoxNum>
+                        <BoxText>{card.text}</BoxText>
+                    </Box>
+                ))}
+            </Boxes>
+        </Section>
+
+        {/*<Section>*/}
+        {/*    <SectionDivider divider/>*/}
+        {/*    <SectionTitle>CryptoverseWeb3 Community Accomplishments</SectionTitle>*/}
+        {/*    <title></title>*/}
+        {/*    <Boxes>*/}
+        {/*        {data1.map((card, index) => (*/}
+        {/*            <Box key={index}>*/}
+        {/*                <BoxNum>{card.number}+</BoxNum>*/}
+        {/*                <BoxText>{card.text}</BoxText>*/}
+        {/*            </Box>*/}
+        {/*        ))}*/}
+        {/*    </Boxes>*/}
+        {/*</Section>*/}
+
+        <Section>
+            <SectionDivider divider/>
+            <SectionTitle>Thecyberworld Community Accomplishments</SectionTitle>
+            <Boxes>
+                {data2.map((card, index) => (
+                    <Box key={index}>
+                        <BoxNum>{card.number}+</BoxNum>
+                        <BoxText>{card.text}</BoxText>
+                    </Box>
+                ))}
+            </Boxes>
+        </Section>
     </>
 );
 
