83bbc0f07919a6554b44a1adb8ccb4a661284966
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-20 22:55:10 +0530

    Update HeroStyles.js

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components';
 
+export const DangerButton = styled.button`
+  background: transparent;
+  padding: 20px 60px 20px 60px;
+  font-size: 20px;
+  color: #ff8100;
+  border-width: 1px;
+  border-color: #545454;
+  border-style: solid;
+  cursor: pointer;
+  border-radius: 10px;
+
+  &:hover {
+    background: #ff8100;
+    color: #0e0e0e;
+  }
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    padding: 10px 30px 10px 30px;
+    font-size: 15px;
+    border-width: 1px;
+    cursor: pointer;
+    border-radius: 10px;
+  }
+
+`
 export const LeftSection = styled.div`
   width: 100%;
   @media ${(props) => props.theme.breakpoints.sm} {
@@ -9,7 +34,7 @@ export const LeftSection = styled.div`
 
     margin: 0 auto;
   }
-  
+
   @media ${(props) => props.theme.breakpoints.md} {
     width: 100%;
     display: flex;
